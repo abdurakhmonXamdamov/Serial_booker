2adba5efea144cbe817852710df5ff4b6747f9e6
adyuva <xamdamov.18.16@gmail.com>
2024-09-19 21:31:01 +0300

    made deadline for sales

--- JS/app.js
+++ JS/app.js
@@ -8,7 +8,7 @@ window.addEventListener('DOMContentLoaded', ()=>{
         
         
         // Loader 
-        
+
         setTimeout(() => {
             loader.style.opacity = '0';
             setTimeout(()=>{
@@ -50,4 +50,62 @@ window.addEventListener('DOMContentLoaded', ()=>{
             })
         }
     })
+
+    // Timer 
+
+    const deadline = '2025-02-16'
+
+    // console.log();
+
+    function getTimeRemaning(endTime){
+
+        const timer = Date.parse(endTime) - new Date(),
+        days = Math.floor(timer / (24 * 60 * 60 * 1000)),
+        hours = Math.floor((timer / (60 * 60 * 1000))  % 24),
+        minutes = Math.floor((timer / (60 * 1000))  % 60),
+        seconds = Math.floor((timer / 1000)  % 60);
+
+        return {
+            timer, 
+            days, 
+            hours,
+            minutes,
+            seconds   
+        }
+    }
+
+    function takeZero(num){
+        if(num >= 0 && num < 10){
+            return `0${num}`
+        }else{
+            return num
+        }
+    }
+
+    function setTime(selector, endTime){
+        const timeBlock = document.querySelector(selector),
+            days = timeBlock.querySelector('#days'),
+            hours = timeBlock.querySelector('#hours'),
+            minutes = timeBlock.querySelector('#minutes'),
+            seconds = timeBlock.querySelector('#seconds'),
+            timeInterVal = setInterval(updateTime, 1000)
+
+            updateTime()
+
+        function updateTime(){
+            const timeHold = getTimeRemaning(endTime)
+
+            days.innerHTML = takeZero(timeHold.days)
+            hours.innerHTML = takeZero(timeHold.hours)
+            minutes.innerHTML = takeZero(timeHold.minutes)
+            seconds.innerHTML = takeZero(timeHold.seconds)
+
+            if(timeHold.timer <= 0){
+                clearInterval(timeInterVal)
+            }
+        }
+    }
+
+    setTime('.timer', deadline)
+
 })
