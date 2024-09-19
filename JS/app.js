6393a9f69c2ee567ce429f9a125b17e8f37e284e
adyuva <xamdamov.18.16@gmail.com>
2024-09-19 20:02:25 +0300

    some changes made

--- JS/app.js
+++ JS/app.js
@@ -2,36 +2,52 @@ window.addEventListener('DOMContentLoaded', ()=>{
 
     const tabheaderParent = document.querySelector('.tabheader__items'),
         tabheaderItems = document.querySelectorAll('.tabheader__item'),
-        tabContents = document.querySelectorAll('.tabcontent')
+        tabContents = document.querySelectorAll('.tabcontent');
+        
+        let = loader = document.querySelector('.loader');
+        
+        
+        // Loader 
+        
+        setTimeout(() => {
+            loader.style.opacity = '0';
+            setTimeout(()=>{
+                loader.style.display = 'none'
+            }, 2000)
+        }, 2000); 
+        
+        
 
+    // Tab_Content 
 
-        function hideContent(){
-            tabContents.forEach((items) => {
-                items.classList.remove('show', 'fade')
-                items.classList.add('hide')
-            })
-            tabheaderItems.forEach(tabs => tabs.classList.remove('tabheader__item_active'))
-        }
-
-        function showContent(ind = 0){
-            tabContents[ind].classList.add('show', 'fade')
-            tabContents[ind].classList.remove('hide')
-            tabheaderItems[ind].classList.add('tabheader__item_active')
-
-        }
-
-        hideContent()
-        showContent()
-
-        tabheaderParent.addEventListener('click', (e)=>{
-            const target = e.target
-            if(target.classList.contains('tabheader__item')){
-                tabheaderItems.forEach((item, idx) =>{
-                   if(target == item){
-                    hideContent()
-                    showContent(idx)
-                   }
-                })
-            }
+    function hideTableContent(){
+        tabContents.forEach((items)=>{
+            items.classList.add('hide')
+            items.classList.remove('show', 'fade')
         })
+
+        tabheaderItems.forEach((item) => item.classList.remove('tabheader__item_active'))
+    }
+
+    function showTableContent(index = 0){
+        tabContents[index].classList.add('show', 'fade')
+        tabContents[index].classList.remove('hide')
+        tabheaderItems[index].classList.add('tabheader__item_active')
+    }
+
+    hideTableContent()
+    showTableContent()
+
+    tabheaderParent.addEventListener('click', (e)=>{
+        const target = e.target;
+
+        if(target.classList.contains('tabheader__item')){
+            tabheaderItems.forEach((item, idx) => {
+                if(target == item){
+                    hideTableContent()
+                    showTableContent(idx)
+                }
+            })
+        }
+    })
 })
